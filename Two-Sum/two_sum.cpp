0c9c702ee127e3161b311d0fe2b5665873b0be6f
sneha tiwari <k.sneha.twr@gmail.com>
2026-09-22 16:20:26 +0530

    Create two_sum.cpp

---
+++ Two-Sum/two_sum.cpp
@@ -0,0 +1,18 @@
+#include<iostram>
+using namespace std;
+int main{
+  int s=0;
+int e=n-1;
+while(s<e) {
+if(nums[s] + nums[e] == target)
+  return new int[]{i,j};
+} else if(sum>target){
+e--;
+else {
+(sum < target) {
+  s++;
+}
+return new int[]{-1,-1};
+}
+}
+
